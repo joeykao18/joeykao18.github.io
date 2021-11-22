6bb03fbff7e3ce64eabc10a053b2c9e5f10cbbb0
joeykao18 <94812419+joeykao18@users.noreply.github.com>
2021-11-22 09:42:23 +0800

    Add files via upload

---
+++ url.js
@@ -0,0 +1,14 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 除最後一項外，其餘項目用逗號分隔
+ * 2. 網址請用引號包起來
+ */
+
+
+const urls = [
+	'https://docs.google.com/forms/d/e/1FAIpQLSeRITqUf_gkRwNukxSfHDXD5Mbu6MvZ8D66Lf0KbkSpwIW0fw/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeBNoHEE_U53LTNoSZQdT8hCm7Bbb8sGA39mz2IWP6ab3i7VA/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeBNoHEE_U53LTNoSZQdT8hCm7Bbb8sGA39mz2IWP6ab3i7VA/viewform?usp=sf_link'
+];
